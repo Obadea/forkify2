398afc06ee9cd9eeb12ea8121d87fd5489a3c748
obadea <obadeagbenga@gmail.com>
2023-11-16 13:52:42 +0100

    fixed an error' git commit -m 'fixed an error'
    '

--- src/js/controller.js
+++ src/js/controller.js
@@ -127,5 +127,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
+
+init();
