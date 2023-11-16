9cb71d2f4bc43c49bbd21b70aed051f787740ea7
obadea <obadeagbenga@gmail.com>
2023-11-16 11:07:55 +0100

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+// ('https://forkify-api.herokuapp.com/api/v2/recipes/');
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `89d00231-6ff3-4c4d-b561-4633534947c1`;
+export const MODAL_CLOSE_SEC = 2.5;
