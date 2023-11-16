2dafb320c9609fc1e81e5ac52ee7f1c198f15074
obadea <obadeagbenga@gmail.com>
2023-11-16 13:48:48 +0100

    Initial commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -119,10 +119,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -133,4 +129,3 @@ const init = function () {
   addRecipeView.addHandlerUpload(controlAddRecipe);
   newFeature();
 };
-init();
